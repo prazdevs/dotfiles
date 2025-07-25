70d4aaae9774d8f3806207b5a11680c0f421eae3
praz <sachabouillez@gmail.com>
2025-07-25 19:47:04 +0200

    feat: setup fnm fish integration

--- programs/fish.nix
+++ programs/fish.nix
@@ -5,6 +5,7 @@
     enable = true;
     shellInit = ''
       set fish_greeting
+      fnm env --use-on-cd --version-file-strategy recursive --corepack-enabled --resolve-engines --shell fish | source
     '';
     loginShellInit = ''
       fish_add_path --move --prepend $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin
